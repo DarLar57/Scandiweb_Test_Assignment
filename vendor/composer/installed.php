ced52aca2217140fb0e0834322b960a05479c5d1
DarLar_75 <106186122+darlar57@users.noreply.github.com>
2023-04-24 11:27:22 +0200

    Signed-off-by: DarLar_75 <106186122+darlar57@users.noreply.github.com>

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '230c924626e687d776a4e9294032af83292ce3c8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '230c924626e687d776a4e9294032af83292ce3c8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
